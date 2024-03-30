ff7a6263c6e0cf2ace19c6d7d9fcbf16c8c7a77e
MrKoalaGAMER <162047337+MrKoalaGAMER@users.noreply.github.com>
2024-03-30 02:08:20 -0300

    Create CPS.java

---
+++ CPS.java
@@ -0,0 +1,30 @@
+@CheckInfo(
+   type = CheckType.AUTO_CLICKER,
+   friendlyName = "CPS",
+   version = CheckVersion.RELEASE,
+   logData = true,
+   
+)
+public class CPS extends PacketCheck {
+   private int ticks;
+   private int cps;
+
+   public void handle(VPacket packet, long p1) {
+      if (!this.playerData.isDigging() && !this.playerData.isDiggingNew() && this.player.getGameMode() != GameMode.CREATIVE) {
+         if (packet instanceof VPacketPlayInFlying) {
+            if (++this.ticks >= 20) {
+               this.debug("cps=" + this.cps);
+               if ((double)this.cps > 35.0D && !this.playerData.isDiggingNew() && !this.playerData.isDigging()) {
+                  this.handleViolation("cps=" + this.cps);
+               }
+
+               this.cps = 0;
+               this.ticks = 0;
+            }
+         } else if (packet instanceof VPacketPlayInUseEntity && ((VPacketPlayInUseEntity)packet).getAction() == VPacketPlayInUseEntity.EntityUseAction.ATTACK) {
+            ++this.cps;
+         }
+
+      }
+   }
+}
